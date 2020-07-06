7cb04d4067371dc79db9d84f3ceb3fde71083ec9
poesterskov <ph.holm@gmail.com>
2020-07-06 21:06:46 +0200

    Installation of django

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\philip\documents\apps\django_tutorial\django_blog\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
